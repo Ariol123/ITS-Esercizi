5dac64c4dc4b896a370fdc1553a4f4f8da4c6ab5
Ariol123 <a.spirollari@its-ictacademy.com>
2025-02-28 15:45:02 +0100

    a

--- prova 2.py
+++
@@ -1,4 +0,0 @@
-def display_message() ->str:
-   print ("Ciao ce la sto a mette tutta anche quandi il prof non c'e!")
-
-display_message()
---
+++ prova.py
@@ -0,0 +1,5 @@
+frase:str = input("Inserisci una frase: ")
+print(type(frase))
+
+n:int = int(input("Inserire un numero: "))
+print(type(n))
