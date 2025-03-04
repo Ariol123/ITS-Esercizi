53254daa91e8c02b0134b21aa71c12c61bc189be
Ariol123 <a.spirollari@its-ictacademy.com>
2025-03-04 13:49:32 +0100

    2

---
+++ esercizio_8_1.py
@@ -0,0 +1,17 @@
+def display_message():
+    print("Ciao ho inparato Python")
+display_message()
+
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+title="Alice in Wonderland"
+favorite_book(title)
+favorite_book("la bibbia")
+
+
+def make_shirt(size,text):
+    print(f"My shirt is {size} and Do; \'{text}\'")
+size= "XL"
+text = "Riccardo cuor di Leone"
+make_shirt(size,text)
